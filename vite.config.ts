0847c070ac9d9c15b01ddfe8a10dde6d73258678
chris-codes-things <chris.codes.thingz@gmail.com>
2025-06-05 08:55:03 -1000

    Change repo to NourishHI

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/food-bank-bites-app/',
+  base: '/nourish-hawaii/',
   server: {
     host: "::",
     port: 8080,
